89746cc6cdc2b254d6e1dfb9d23fcb6cf5364cc7
Martin Sehnoutka <msehnout@redhat.com>
2017-05-30 10:10:13 +0200

    Skript by mel byt nyni nezavisly na zemi

--- kajak/stahni_kilometraz.sh
+++ kajak/stahni_kilometraz.sh
@@ -4,7 +4,8 @@
 while read -r reka
 do
     NAZEV=$(echo "${reka}" | sed 's|.*\.cz/\w*/||' | sed 's|\.aspx.*||')
-    echo "Stahuju ${reka} - ${NAZEV}"
+    ZEME=$(echo "${reka}" | sed 's|.*\.cz/||' | sed 's|/.*\.aspx.*||')
+    echo "Stahuju ${reka} - ${NAZEV} (${ZEME})"
     curl "${reka}&kilo=kilom&kfoto=1&symbol=1" -o "${NAZEV}"
     # Vytvor hlavicku
     cat > "${NAZEV}.html" << EOF
@@ -20,7 +21,7 @@ EOF
     # Stahni kilometraz
     sed -n -e '/ctl00_ctl00_ContentPlaceHolder1_Kilometraz_ctl00_tKilom/,/<\/table>/ p' "${NAZEV}" >> "${NAZEV}.html"
     # Nahrad relativni cesty absolutnima
-    sed -i 's|src="foto|src="http://www.raft.cz/rakousko/foto|' "${NAZEV}.html"
+    sed -i "s|src=\"foto|src=\"http://www.raft.cz/${ZEME}/foto|g" "${NAZEV}.html"
     sed -i 's|src="../gif|src="http://www.raft.cz/gif|g' "${NAZEV}.html"
     cat >> "${NAZEV}.html" << EOF
 </body>
