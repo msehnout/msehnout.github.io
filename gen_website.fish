70fc8c392a7f1b254d33820e941d4f1dac3bfc9b
Martin Sehnoutka <msehnout@redhat.com>
2017-05-30 10:01:35 +0200

    Site generation script

--- gen_website.fish
+++ gen_website.fish
@@ -1,3 +1,21 @@
 #!/usr/bin/fish
 
-echo "Not yet implemented"
+# Generate kayaking notes
+pushd kajak
+
+cat top.html > index.html
+echo "<ul>" >> index.html
+
+for SRC in *.md
+    set RIVER (echo "$SRC" | sed 's|\.md$||')
+    echo "Running loop for $RIVER"
+    echo "   <li><a href=\"$RIVER.html\">$RIVER</a></li>" >> index.html
+    cat top.html > "$RIVER.html"
+    pandoc -f markdown -t html "$SRC" >> "$RIVER.html"
+    cat bottom.html >> "$RIVER.html"
+end
+
+echo "</ul>" >> index.html
+cat bottom.html >> index.html
+
+popd
